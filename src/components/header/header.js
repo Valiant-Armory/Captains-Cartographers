4f4cb7d6ac33492056496cdfc71e7de99e3a5009
kasey <kaseycmcgee@gmail.com>
2023-03-11 17:27:08 -0700

    save

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,8 +4,18 @@ import './header.css';
 
 function Header() {
   return (
-    <div className="main-page-header">
+    <>
+    <div className="main-page-header-img">
+    
+    <div className="main-page-header-nav">
+    <button className="nav-button" title="Account">Account</button>
+      <button className="nav-button" title="Test">Test</button>
+      <button className="nav-button" title="Test">Test</button>
+      <button className="nav-button" title="Test">Test</button>
+      <button className="nav-button" title="Test">Test</button>
     </div>
+    </div>
+    </>
   );
 }
 
